c242620635251c31a551ec06f41aa8989231d212
Jared Barboza <jared.m.barboza@gmail.com>
2012-04-12 00:23:34 -0400

    RequireJS-Refactor - adds an asset loader and a simple progressbar

--- src/scripts/boot.js
+++ src/scripts/boot.js
@@ -4,27 +4,54 @@
 requirejs.config({
   paths: {
     'jquery' : 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min'
-  }
+  },
+  urlArgs: "bust=" + (new Date()).getTime()
 });
 require(
     [
         "jquery",
-        "modules/engine.game",
         "modules/engine.map",
         "modules/engine.events",
-        "modules/engine.debug",
         "modules/engine.config",
+        "modules/loader.assets",
+        "modules/engine.game",
+        "modules/engine.debug",
         "modules/builder.home",
         "modules/builder.trees"
     ],
-    function($, gameEngine, mapEngine, eventEngine, debugEngine, configEngine) {
+    function($, mapEngine, eventEngine, configEngine, assetLoader, gameEngine) {
         configEngine.set("shouldDebug", true);
+
+        // create a list of the assets we want to load
+        var assets = [
+            "images/grass.png",
+            "images/dirt.png",
+            "images/icecream.png",
+            "images/tree.png"
+        ];
+
         $(function () {
+            var progressBar = assetLoader.createProgressBar($('#loaderProgress')[0], 0, assets.length);
+
             eventEngine.sub(gameEngine.events.MOUSE_DOWN, function(canvas, context, e) {
                 debugEngine.log("MOUSE DOWN");
                 debugEngine.log(e);
             });
 
-            mapEngine.setCurrentLevel("levelOne").renderTo(gameEngine.getCanvas(), gameEngine.getContext());
+            // don't render until all the assets have been loaded
+            eventEngine.sub(assetLoader.events.ALL_ASSETS_LOADED, function(e) {
+                $("#loaderProgress").hide();
+                mapEngine.setCurrentLevel("levelOne")
+                         .renderTo(gameEngine.getCanvas(), gameEngine.getContext());
+            });
+
+            eventEngine.sub(assetLoader.events.ASSET_LOAD, function(itemCount) {
+                // update the progressbar
+                progressBar.update(itemCount);
+            });
+
+            // prep the assetLoader and start downloading
+            assetLoader.queueAssets(assets);
+            assetLoader.downloadAll();
         });
     });
---
+++ src/scripts/modules/loader.assets.js
@@ -0,0 +1,109 @@
+define(
+    [
+        "modules/engine.events",
+        "modules/engine.debug"
+    ],
+    function(eventEngine, debugEngine) {
+        var ProgressBar = function(parent, min, max){
+            this.element = document.createElement('progress');
+            parent.appendChild(this.element);
+
+            // initialize the progress bar at zero percent
+            this.element.setAttribute('value', min);
+            this.element.setAttribute('max', max);
+
+            // for this example, progress will be some value [0,1]
+            this.update = function (progress) {
+                this.element.setAttribute('value', (progress/max) * 100);
+            };
+        };
+
+        var AssetManager = function() {
+            var downloadQueue = [],
+                cache = {},
+                counts = {
+                    successes: 0,
+                    fails: 0,
+                    sum: function() {
+                        return this.successes + this.fails;
+                    }
+                },
+                parent = this,
+                signalAllAssetsLoaded = function(){
+                    eventEngine.pub(parent.events.ALL_ASSETS_LOADED, this, [downloadQueue]);
+                };
+
+            this.queueAsset = function(path) {
+                downloadQueue.push(path);
+            };
+            this.queueAssets = function(assets) {
+                for(var i = -1, l = assets.length; ++i < l;) {
+                    this.queueAsset(assets[i]);
+                }
+            }
+            this.doneYet = function() {
+                return (downloadQueue.length === counts.sum());
+            };
+
+            this.downloadAll = function() {
+                debugEngine.log("starting download");
+                if (downloadQueue.length === 0) {
+                    signalAllAssetsLoaded();
+                }
+                for (var i = -1, l = downloadQueue.length; ++i < l;) {
+                    var path = downloadQueue[i],
+                        allDoneTrigger = signalAllAssetsLoaded,
+                        manager = this,
+                        counter = counts;
+
+                    debugEngine.log("downloading " + path);
+
+                    function downloadImage(src, successCallback, failCallback) {
+                        var image = new Image();
+                        image.addEventListener("load", function(e) {
+                            successCallback(e, image);
+                        }, false);
+                        image.addEventListener("error", function(e) {
+                            failCallback(e, image);
+                        }, false);
+                        image.src = src;
+                        return image;
+                    }
+
+                    cache[path] = downloadImage(path,
+                        function onSuccess(e, img) {
+                            counter.successes++;
+                            var args = [ counter.sum(), img.src, "ok", e ];
+
+                            eventEngine.pub(manager.events.ASSET_LOAD, manager, args);
+                            if( manager.doneYet() ) {
+                                allDoneTrigger();
+                            }
+                        },
+                        function onFail(e, img) {
+                            counter.fails++;
+                            var args = [ counter.sum(), img.src, "fail", e ];
+
+                            eventEngine.pub(manager.events.ASSET_LOAD, manager, args);
+                            if( manager.doneYet() ) {
+                                allDoneTrigger();
+                            }
+                        });
+                }
+            };
+
+            this.getAsset = function(path) {
+                return cache[path];
+            };
+
+            this.createProgressBar = function(parent, min, max) {
+                return new ProgressBar(parent, min || 0, max || 100);
+            };
+
+            this.events = {
+                ASSET_LOAD: "assetLoad",
+                ALL_ASSETS_LOADED: "allAssetsLoaded"
+            };
+        };
+        return new AssetManager();
+    });
