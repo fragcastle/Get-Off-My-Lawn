1ffb339223038fef4cfa40b061c6f551b0d36fe3
John Bubriski <bubriski@gmail.com>
2012-04-12 13:09:35 -0400

    Fixed #2 - Trees now handle the TILE_DRAW event and draw properly.

--- src/scripts/modules/builder.trees.js
+++ src/scripts/modules/builder.trees.js
@@ -1,11 +1,15 @@
 require(
-    ["modules/engine.map", "modules/engine.events", "modules/engine.game"],
-    function(mapEngine, eventEngine, gameEngine) {
+    ["modules/engine.map", "modules/engine.events", "modules/engine.game", "modules/engine.debug",],
+    function(mapEngine, eventEngine, gameEngine, debugEngine) {
+        var _tree = null;
+            _treePositions = null;
+        
         function createImage(path) {
             var x = new Image();
             x.src = path;
             return x;
         }
+        
         function randomNumbers(limit) {
             var nums = [];
             for( var i = -1, l = mapEngine.getCurrentMap().data.length; ++i < l; ) {
@@ -19,29 +23,34 @@ require(
             }
             return nums;
         }
+        
         function inArray(arr, val) {
             for(var i = -1, j = arr.length; ++i < j;)
                 if(arr[i] === val) return true;
             return false;
         }
-        eventEngine.sub(mapEngine.events.LOAD_COMPLETE, function(){
+        
+        eventEngine.sub(mapEngine.events.LEVEL_LOADED, function() {
+            debugEngine.log("LOAD_COMPLETE - treeEngine");
+            
             var map = mapEngine.getCurrentMap();
 
             var context = gameEngine.getContext();
             var canvas  = gameEngine.getCanvas();
 
-            // loop through the map data
-            // for each tile, determine
-            // the type of tile and if we should render a tree
-            var tree = createImage('images/tree.png'),
-                treePositions = randomNumbers( mapEngine.getCurrentMap().treeFactor * 100 );
-            mapEngine.tileLoop(function(row, col, index){
-                if( inArray(treePositions, index) ) {
-                    var tileType = map.data.substr(index,1);
-                    var pos = mapEngine.translatePosition(row, col, tree);
-
-                    context.drawImage(tree, pos.x, pos.y, tree.width, tree.height);
-                }
-            });
+            // Generate the position of the trees
+            _tree = createImage('images/tree.png');
+            _treePositions = randomNumbers( mapEngine.getCurrentMap().treeFactor * 100 );
+        });
+        
+        eventEngine.sub(mapEngine.events.TILE_DRAW, function(index, row, col) {
+            debugEngine.log("TILE_DRAW - treeEngine");
+            
+            var context = gameEngine.getContext();
+            
+            if (inArray(_treePositions, index)) {
+                var pos = mapEngine.translatePosition(row, col, _tree);
+                context.drawImage(_tree, pos.x, pos.y, _tree.width, _tree.height);
+            }
         });
     });
