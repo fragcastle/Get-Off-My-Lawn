fbb52d2f60a58efe3d2824cca764601ef4b4108d
John Bubriski <bubriski@gmail.com>
2012-04-19 13:37:31 -0400

    Fixed the render issue with the house.  It was setting up its position to early, before translations were being done.

--- src/scripts/modules/builder.home.js
+++ src/scripts/modules/builder.home.js
@@ -1,22 +1,22 @@
 require(
     ["modules/engine.map", "modules/engine.events", "modules/engine.game", 'modules/engine.debug'],
     function(mapEngine, eventEngine, gameEngine, debugEngine) {
-        var _home, _homeImage, _homePosition;
-        
+        var _home, _homeImage;
+
         eventEngine.sub(mapEngine.events.LEVEL_LOADED, function() {
             var map = mapEngine.getCurrentMap();
             _home = map.entities.home;
             _homeImage = new Image();
             _homeImage.src = _home.texture;
-            
-            // center the image within the tile
-            _homePosition = mapEngine.translatePosition(_home.row, _home.col, _homeImage);
         });
-        
+
         eventEngine.sub(mapEngine.events.BUILDING_RENDER, function(index, row, col) {
             if (_home.row === row && _home.col === col) {
+                // center the image within the tile
+                var homePosition = mapEngine.translatePosition(_home.row, _home.col, _homeImage);
+
                 var context = gameEngine.getContext();
-                context.drawImage(_homeImage, _homePosition.x, _homePosition.y, _homeImage.width, _homeImage.height);
+                context.drawImage(_homeImage, homePosition.x, homePosition.y, _homeImage.width, _homeImage.height);
             }
         });
     });
