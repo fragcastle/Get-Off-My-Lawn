8ba9e065183e3cdd6b8879ea050107f10c2f94cf
Jared Barboza <jared.m.barboza@gmail.com>
2012-04-23 00:59:29 -0400

    Issue #14 one more issue from the stupid rebase

--- src/scripts/modules/engine.util.js
+++ src/scripts/modules/engine.util.js
@@ -30,6 +30,24 @@ define(function() {
         propability: function (factor) {
             var randomNumber = Math.floor(Math.random() * 100);
             return (randomNumber < factor * 100);
-        }
+        },
+        translatePosition: function(canvas, tileSize, row, col, entity) {
+            var pos = {
+                x: ((row - col) * tileSize.height) + Math.floor((canvas.width / 2) - (tileSize.width / 2)),
+                y: Math.floor( (row + col) * (tileSize.height / 2) )
+            };
+
+            if (entity) {
+                if (entity.width != tileSize.width) {
+                    pos.x += tileSize.width / 2 - entity.width / 2;
+                }
+
+                if (entity.height != tileSize.height) {
+                    pos.y -= entity.height - tileSize.height / 2;
+                }
+            }
+
+            return pos;
+        },
     }
 });
