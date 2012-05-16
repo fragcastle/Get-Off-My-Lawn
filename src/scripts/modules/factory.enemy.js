9c7f166a44396454ba3f8a844dfa710258643c57
John Bubriski <bubriski@gmail.com>
2012-05-16 00:50:38 -0400

    Spacing cleanup

--- src/scripts/modules/factory.enemy.js
+++ src/scripts/modules/factory.enemy.js
@@ -6,91 +6,92 @@ define(
     , 'modules/engine.debug'
     ],
     function(assetLoader, util, enemiesJson, debugEngine){
-      var keyFrames = {
-          ready: 'ready',
-          walk: 'walk',
-          jump: 'jump',
-          flinch: 'flinch',
-          swing: 'swing',
-          kick: 'kick',
-          jumpswing: 'jumpswing',
-          jumpkick: 'jumpkick',
-          dizzy: 'dizzy'
-      };
-      var orientations = { left: 0, right: 1 };
-
-      var enemyTemplates = {};
-      for(var i = -1; ++i < enemiesJson.enemies.length; ) {
-        var enemyTemplate = enemiesJson.enemies[i];
-        enemyTemplates[enemyTemplate.name] = enemyTemplate;
-      }
-
-      return {
-        enemies: enemiesJson.enemies,
-        new: function(templateName, index) {
-          var template = enemyTemplates[templateName];
-
-          return {
-              index: index,
-              movingToIndex: index,
-              position: { x: 0, y: 0 },
-              
-              lastUpdate: Date.now(),
-              
-              rateOfMovement: 2000,
-              
-              template: template,
-              frame: util.random(template.keyFrames),
-              orientation: util.coinFlip() ? 0 : 1,
-
-              life: 100,
-              
-              setFrame: function(frameName) {
-                  this.frame = this.template.keyFrames[frameName];
-              },
-
-              // used when the map engine is done rendering other things to move an enemy
-              draw: function (canvas, context, tileDimensions, row, col, tilePos, currentFrame) {
-                var actionFrame = this.template.keyFrames[ this.frame ];
-
-                var frameChange = currentFrame * this.template.size.width;
-
-                if (this.orientation === orientations.left) {
-                    context.save();
-                    context.translate(tilePos.x + 80, tilePos.y - 40);
-                    context.scale(-1, 1);
-
-                    pos = { x: 0, y: 0 };
-                } else {
-                    pos = util.entityRowColToPoint(canvas.width, tileDimensions, row, col, this.template.size);
-                }
-
-                // Image, image x, image y, image width, image height, map x, map y, map width, map height
-                context.drawImage(this.template.image, actionFrame.x + frameChange, actionFrame.y, this.template.size.width, this.template.size.height, pos.x, pos.y, this.template.size.width, this.template.size.height);
-
-                if (this.orientation === orientations.left) {
-                    context.restore();
-                }
-              },
-                  
-              update: function (mapEngine) {
-                var now = Date.now();
-                var delta = now - this.lastUpdate;
-
-                if (delta >= this.rateOfMovement) {
-                  this.lastUpdate = now;
-
-                  var map = mapEngine.getCurrentMap();
-                  var potentialMove = util.random(mapEngine.getEligibleMoves(this.index));
-                  var tileType = map.data[potentialMove];
-
-                  if (mapEngine.tileTypes[tileType].isWalkable) {
-                      this.index = this.movingToIndex;
-                      this.index = potentialMove;
-                  }
-                }
-              }
-          }
+        var keyFrames = {
+            ready: 'ready',
+            walk: 'walk',
+            jump: 'jump',
+            flinch: 'flinch',
+            swing: 'swing',
+            kick: 'kick',
+            jumpswing: 'jumpswing',
+            jumpkick: 'jumpkick',
+            dizzy: 'dizzy'
+        };
+        var orientations = { left: 0, right: 1 };
+  
+        var enemyTemplates = {};
+        
+        for(var i = -1; ++i < enemiesJson.enemies.length; ) {
+            var enemyTemplate = enemiesJson.enemies[i];
+            enemyTemplates[enemyTemplate.name] = enemyTemplate;
         }
-      }
+  
+        return {
+            enemies: enemiesJson.enemies,
+            new: function(templateName, index) {
+                var template = enemyTemplates[templateName];
+      
+                return {
+                    index: index,
+                    movingToIndex: index,
+                    position: { x: 0, y: 0 },
+                    
+                    lastUpdate: Date.now(),
+                    
+                    rateOfMovement: 2000,
+                    
+                    template: template,
+                    frame: util.random(template.keyFrames),
+                    orientation: util.coinFlip() ? 0 : 1,
+      
+                    life: 100,
+                    
+                    setFrame: function(frameName) {
+                        this.frame = this.template.keyFrames[frameName];
+                    },
+      
+                    // used when the map engine is done rendering other things to move an enemy
+                    draw: function (canvas, context, tileDimensions, row, col, tilePos, currentFrame) {
+                        var actionFrame = this.template.keyFrames[ this.frame ];
+        
+                        var frameChange = currentFrame * this.template.size.width;
+        
+                        if (this.orientation === orientations.left) {
+                            context.save();
+                            context.translate(tilePos.x + 80, tilePos.y - 40);
+                            context.scale(-1, 1);
+        
+                            pos = { x: 0, y: 0 };
+                        } else {
+                            pos = util.entityRowColToPoint(canvas.width, tileDimensions, row, col, this.template.size);
+                        }
+        
+                        // Image, image x, image y, image width, image height, map x, map y, map width, map height
+                        context.drawImage(this.template.image, actionFrame.x + frameChange, actionFrame.y, this.template.size.width, this.template.size.height, pos.x, pos.y, this.template.size.width, this.template.size.height);
+        
+                        if (this.orientation === orientations.left) {
+                            context.restore();
+                        }
+                    },
+                        
+                    update: function (mapEngine) {
+                        var now = Date.now();
+                        var delta = now - this.lastUpdate;
+        
+                        if (delta >= this.rateOfMovement) {
+                            this.lastUpdate = now;
+          
+                            var map = mapEngine.getCurrentMap();
+                            var potentialMove = util.random(mapEngine.getEligibleMoves(this.index));
+                            var tileType = map.data[potentialMove];
+          
+                            if (mapEngine.tileTypes[tileType].isWalkable) {
+                                this.index = this.movingToIndex;
+                                this.index = potentialMove;
+                            }
+                        }
+                    }
+                }
+            }
+        }
   });
